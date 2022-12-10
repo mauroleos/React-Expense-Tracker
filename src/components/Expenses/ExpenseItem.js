dbe5c055585a7667aebc87d150b17201843d0eab
Mauro Leos <mauroleos@google.com>
2022-12-10 10:01:47 -0800

    Cleaned up files and added subfolders

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -1,7 +1,7 @@
 import React from "react"; // optional for moderm applications
 
 import ExpenseDate from "./ExpenseDate";
-import Card from "./Card";
+import Card from "../UI/Card";
 import "./ExpenseItem.css";
 
 
--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -2,7 +2,7 @@ import React from "react"; // optional for moderm applications
 
 import ExpenseItem from "./ExpenseItem";
 import "./Expenses.css";
-import Card from "./Card";
+import Card from "../UI/Card";
 
 function Expenses(props) {
   return (
